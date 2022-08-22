ff504838d556613e5675fbd05a5abd46831f74de
Türker Karapetti <trkrkrl53@gmail.com>
2022-08-22 09:43:20 +0300

    commit-37- ilk test-guid

---
+++ BitirmeTests/GuidTests.cs
@@ -0,0 +1,31 @@
+﻿using Business.Concrete;
+using Business.Constants;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BitirmeEticaretApp.Tests
+{
+    [TestClass]
+    public class GuidTests
+    {
+
+        [TestMethod]
+        public void Are_Guids_Same()//global unique identifier- eşsiz olacak zaten
+        {
+            Guid guid = Guid.NewGuid();
+            Guid guid2 = Guid.NewGuid();
+
+            Assert.AreNotEqual(guid, guid2);
+
+        }
+        
+
+    }
+}
